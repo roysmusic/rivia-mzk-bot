08b7eb1a9731fe17ade0365ac75bc5246e124c9b
roysmusic <107501496+roysmusic@users.noreply.github.com>
2022-06-22 23:25:36 +0300

    Update languages.py

--- YukkiMusic/plugins/tools/languages.py
+++ YukkiMusic/plugins/tools/languages.py
@@ -25,40 +25,15 @@ def lanuages_keyboard(_):
     keyboard = InlineKeyboard(row_width=2)
     keyboard.row(
         InlineKeyboardButton(
-            text="🏴󠁧󠁢󠁥󠁮󠁧󠁿 English",
-            callback_data=f"languages:en",
+            text="🇹🇷 Türkçe",
+            callback_data=f"languages:tr",
         ),
         InlineKeyboardButton(
-            text="🇮🇳 हिन्दी",
-            callback_data=f"languages:hi",
-        ),
-    )
-    keyboard.row(
-        InlineKeyboardButton(
-            text="🇱🇰 සිංහල",
-            callback_data=f"languages:si",
-        ),
-        InlineKeyboardButton(
-            text="🇦🇿 Azərbaycan",
+            text="🇦🇿 Azerbaycan",
             callback_data=f"languages:az",
         ),
     )
-    keyboard.row(
-        InlineKeyboardButton(
-            text="🇮🇳 ગુજરાતી",
-            callback_data=f"languages:gu",
-        ),
-        InlineKeyboardButton(
-            text="🇹🇷 Türkiye Türkçesi",
-            callback_data=f"languages:tr",
-        ),
-    )
-    keyboard.row(
-        InlineKeyboardButton(
-            text="🐶 Cheems",
-            callback_data=f"languages:cheems",
-        ),
-    )
+    
     keyboard.row(
         InlineKeyboardButton(
             text=_["BACK_BUTTON"],
